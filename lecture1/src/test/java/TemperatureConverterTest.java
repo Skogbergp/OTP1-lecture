380b7d0336e4b49b32e56a34082a3bebbe7a9ac9
Patrik Skogberg <skogbergp@gmail.com>
2025-09-04 12:51:47 +0300

    yr

--- lecture1/src/test/java/TemperatureConverterTest.java
+++ lecture1/src/test/java/TemperatureConverterTest.java
@@ -1,23 +1,23 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
 class TemperatureConverterTest {
 
     TemperatureConverter converter = new TemperatureConverter();
 
-    @org.junit.jupiter.api.Test
+    @Test
     void fahrenheitToCelsius() {
         assertEquals(0, converter.fahrenheitToCelsius(32));
-        assertEquals(32.22222222222222, converter.fahrenheitToCelsius(90));
+        assertEquals(32.222222222222222, converter.fahrenheitToCelsius(90));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void celsiusToFahrenheit() {
         assertEquals(212, converter.celsiusToFahrenheit(100));
         assertEquals(-130, converter.celsiusToFahrenheit(-90));
         assertEquals(32, converter.celsiusToFahrenheit(0));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isExtremeTemperature() {
         assertFalse(converter.isExtremeTemperature(32));
         assertTrue(converter.isExtremeTemperature(90));
