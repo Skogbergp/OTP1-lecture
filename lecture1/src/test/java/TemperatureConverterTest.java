2b98f5a24fa70f34bff7345681db8959d4602c6b
skogbergP <SkogbergP@gmail.com>
2025-09-09 09:18:25 +0300

    Refactor pom.xml to remove duplicate JUnit dependencies and update TemperatureConverterTest setup

--- lecture1/src/test/java/TemperatureConverterTest.java
+++ lecture1/src/test/java/TemperatureConverterTest.java
@@ -1,8 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 class TemperatureConverterTest {
+    TemperatureConverter converter;
+    @BeforeEach
+    void setUp() {
+        this.converter = new TemperatureConverter();
 
-    TemperatureConverter converter = new TemperatureConverter();
+    }
+
 
     @Test
     void fahrenheitToCelsius() {
