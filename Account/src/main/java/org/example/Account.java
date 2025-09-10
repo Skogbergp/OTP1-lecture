311c7170365bd6049f3b8611c2c9790518bef001
skogbergP <SkogbergP@gmail.com>
2025-09-10 23:16:53 +0300

    Add Account class and corresponding tests for deposit, withdraw, and balance retrieval

---
+++ Account/src/main/java/org/example/Account.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Account {
+    double balance;
+
+    public Account(){
+        this.balance = 0;
+    }
+    public Account(double initialBalance){
+        this.balance = initialBalance;
+    }
+    public double withdraw(double amount){
+        if (amount > 0 && amount <= this.balance){
+            this.balance -= amount;
+            return amount;
+        }
+        return 0.0;
+
+    }
+    public void deposit(double amount){
+        if(amount > 0){
+            this.balance += amount;
+        }
+
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+}
